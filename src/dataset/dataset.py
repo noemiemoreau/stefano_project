29f441f904b060996040e2720bb8cc218330b705
Celestenono <noemie.moreau96@gmail.com>
2025-12-11 16:52:17 +0100

    adapt dataset to dlbcl data

--- src/dataset/dataset.py
+++ src/dataset/dataset.py
@@ -29,9 +29,7 @@ class ImageDataset(Dataset):
         fn = self.df.iloc[idx][self.fn_col]
         image = np.load(fn)
         image = image[self.which_channels, :, :]
-        print(image.shape)
-        image = tensor(image, dtype=float32)
-        print(image.shape)
+        image = tensor(image, dtype=float32)[0]
         if self.transform != None:
             image = self.transform(image)
         lbl = self.df.iloc[idx][self.lbl_col]
