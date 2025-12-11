c0d259e8b2696310c55e68552d4c459b3f16336e
Celestenono <noemie.moreau96@gmail.com>
2025-12-11 16:33:27 +0100

    adapt dataset to dlbcl data

--- src/dataset/dataset.py
+++ src/dataset/dataset.py
@@ -1,5 +1,5 @@
 from torch.utils.data import Dataset
-from PIL import Image
+import numpy as np
 
 class ImageDataset(Dataset):
     """Dataset class for a .csv containing paths to images
@@ -12,20 +12,22 @@ class ImageDataset(Dataset):
     return_filename -- if True, __getitem__ also returns the filename of the sample
     """
 
-    def __init__(self, df, fn_col = None, lbl_col = None, transform = None, return_filename = False):
+    def __init__(self, df, fn_col = None, lbl_col = None, transform = None, return_filename = False
+                 , which_channels = [list(range(14))]):
         self.df = df
         self.fn_col = fn_col if fn_col != None else df.columns[0]
         self.lbl_col = lbl_col if lbl_col != None else df.columns[1]
         self.transform = transform
         self.return_filename = return_filename
+        self.which_channels = which_channels
 
     def __len__(self):
         return self.df.shape[0]
 
     def __getitem__(self, idx):
         fn = self.df.iloc[idx][self.fn_col]
-        image = Image.open(fn)
-        image = image.convert('RGB')
+        image = np.load(fn)
+        image = image[self.which_channels, :, :]
         if self.transform != None:
             image = self.transform(image)
         lbl = self.df.iloc[idx][self.lbl_col]
