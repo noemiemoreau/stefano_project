cb69cfeffab0115a4d98c325f1989bfc5cc58f2f
Celestenono <noemie.moreau96@gmail.com>
2025-12-12 14:28:41 +0100

    data_explo

--- data_exploration.py
+++ data_exploration.py
@@ -5,7 +5,8 @@ import numpy as np
 image_path = "/projects/ag-bozek/sugliano/dlbcl/data/interim/resnet_imgs/"
 onlyfiles = [f for f in os.listdir(image_path) if os.path.isfile(os.path.join(image_path, f))]
 
-image = np.load(image_path + onlyfiles[103])
-image = image[0, :, :]
-plt.imshow(image)
-plt.savefig('dapi/'+onlyfiles[103][:-3]+"png")
+for f in onlyfiles:
+    image = np.load(image_path + f)
+    image = image[0, :, :]
+    plt.imshow(image)
+    plt.savefig('dapi/'+f[:-3]+"png")
