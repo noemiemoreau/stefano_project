f35841bf0284298de4297f81ccbee91f90fea59f
Celestenono <noemie.moreau96@gmail.com>
2025-12-15 10:48:35 +0100

    re-extraction of trainset

--- data_exploration.py
+++ data_exploration.py
@@ -2,11 +2,27 @@ import matplotlib.pyplot as plt
 import os
 import numpy as np
 
-image_path = "/projects/ag-bozek/sugliano/dlbcl/data/interim/resnet_imgs/"
-onlyfiles = [f for f in os.listdir(image_path) if os.path.isfile(os.path.join(image_path, f))]
+train_df = pd.read_csv("train.csv")
+val_df = pd.read_csv("val.csv")
+test_df = pd.read_csv("test.csv")
 
-for f in onlyfiles:
-    image = np.load(image_path + f)
+os.makedirs("/projects/ag-bozek/nmoreau/dapi_bis")
+
+for sample in train_df:
+    index, filename, label = sample[0], sample[1], sample[2]
+    image = np.load(filename)
     image = image[0, :, :]
+    print(index)
     plt.imshow(image)
-    plt.savefig('dapi/'+f[:-3]+"png")
+    plt.savefig('/projects/ag-bozek/nmoreau/dapi_bis/' + str(index) + "_" +str(label) + ".png")
+
+
+
+# image_path = "/projects/ag-bozek/sugliano/dlbcl/data/interim/resnet_imgs/"
+# onlyfiles = [f for f in os.listdir(image_path) if os.path.isfile(os.path.join(image_path, f))]
+#
+# for f in onlyfiles:
+#     image = np.load(image_path + f)
+#     image = image[0, :, :]
+#     plt.imshow(image)
+#     plt.savefig('dapi/'+f[:-3]+"png")
