25e95417bf4f37fb20c5ca68e142d96b41651490
Celestenono <noemie.moreau96@gmail.com>
2026-01-08 11:39:43 +0100

    debug

--- data_exploration.py
+++ data_exploration.py
@@ -18,12 +18,16 @@ for idx in range(0, train_df.shape[0]):
     image_np = np.load(filename)
     image_np = image_np[which_channels, :, :]
     print(filename, image_np.shape)
-    transformation = transforms.CenterCrop(7000)
+    train_transform = transforms.Compose([
+        transforms.CenterCrop(7000),
+        transforms.Resize((args.img_size, args.img_size)),
+        # transforms.ToTensor(),
+    ])
     image = tensor(image_np, dtype=float32)[0]
     # diff_x = (size_max_x - image.shape[1]) // 2
     # diff_y = (size_max_y - image.shape[2]) // 2
     # transform = transforms.Pad((diff_y, diff_x))
-    image = transformation(image)
+    image = train_transform(image)
     print(image.shape)
     file_name = os.path.basename(filename)
     plt.imshow(image[0, :, :])
