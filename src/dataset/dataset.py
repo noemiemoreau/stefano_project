c8a25190e6580c415fbaac0a36c3cd6f307df35f
Celestenono <noemie.moreau96@gmail.com>
2026-02-05 15:21:05 +0100

    test to understand problem

--- src/dataset/dataset.py
+++ src/dataset/dataset.py
@@ -34,7 +34,7 @@ class ImageDataset(Dataset):
     def __getitem__(self, idx):
         fn = self.df.iloc[idx][self.fn_col]
         #new_filename = "/projects/ag-bozek/nmoreau/dlbcl/data/clipped/" + os.path.basename(fn)[0:3] + "_clipped.npy"
-        image = np.load(new_filename)
+        image = np.load(fn)
         image = image[self.which_channels, :, :]
         image = tensor(image, dtype=float32)[0]
         if self.transform != None:
