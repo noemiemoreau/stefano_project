316c8dfe6e7e99c7a889f0dc2f579ddd98b62cc4
Celestenono <noemie.moreau96@gmail.com>
2026-02-09 16:51:24 +0100

    clean code

--- src/dataset/dataset.py
+++ src/dataset/dataset.py
@@ -19,21 +19,24 @@ class ImageDataset(Dataset):
     transform -- transform to apply
     return_filename -- if True, __getitem__ also returns the filename of the sample
     """
-    def __init__(self, df, fn_col = None, lbl_col = None, transform = None, return_filename = False
-                 , which_channels = [list(range(14))]):
+    def __init__(self, df, fn_col = None, lbl_col = None, transform = None, return_filename = False,
+                 which_channels = [list(range(14))], data_path = None, data_suffixe = None):
         self.df = df
         self.fn_col = fn_col if fn_col != None else df.columns[0]
         self.lbl_col = lbl_col if lbl_col != None else df.columns[1]
         self.transform = transform
         self.return_filename = return_filename
         self.which_channels = which_channels
+        self.data_path = data_path
+        self.data_suffixe = data_suffixe
 
     def __len__(self):
         return self.df.shape[0]
 
     def __getitem__(self, idx):
         fn = self.df.iloc[idx][self.fn_col]
-        # new_filename = "/projects/ag-bozek/sugliano/dlbcl/data/interim/resnet_imgs/" + os.path.basename(fn)[0:3] + "_for_resnet.npy"
+        if self.data_path is not None and self.data_suffixe is not None:
+            fn = self.data_path + os.path.basename(fn)[0:3] + self.data_suffixe
         image = np.load(fn)
         image = image[self.which_channels, :, :]
         image = tensor(image, dtype=float32)[0]
